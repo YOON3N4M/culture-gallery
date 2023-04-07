071c893e3ef8263f6ba7aac33e0612213269db2c
yoon3nam <lgmysn2@gmail.com>
2023-04-08 03:21:01 +0900

    첫 방문 페이지

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,18 +1,34 @@
 import { doc, getDoc, setDoc } from "firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { dbService } from "../fBase";
 
+interface DummyT {
+  book?: any;
+  internationalMovie?: any;
+  tv?: any;
+}
+
 function Welcome() {
+  const [dummy, setDummy] = useState<DummyT>();
+  const [chosenContents, setChosenContents] = useState();
+
   async function getDummyData() {
-    const dummyRef = doc(dbService, "user", "dummy");
+    const dummyRef = doc(dbService, "user", "2iq902jOUIXtIi7OdljMw66DX1x1");
     const docSnap = await getDoc(dummyRef);
-    console.log(docSnap.data());
+    if (docSnap.exists() && docSnap.data() !== undefined) {
+      setDummy(docSnap.data());
+    }
   }
 
   useEffect(() => {
     getDummyData();
   }, []);
+  useEffect(() => {
+    if (dummy !== undefined) {
+      setChosenContents(dummy.internationalMovie);
+    }
+  }, [dummy]);
 
   return <></>;
 }
